d080fdd31c5661d3b10152df2f29f85ddc275c0d
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-10-26 21:26:06 +0000

    A method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+    Copy index_range from the previous task and the following class
+    into your code
+
+    Implement a method named get_page that takes two integer arguments
+    page with default value 1 and page_size with default value 10.
+    You have to use this CSV file (same as the one presented at
+    the top of the project)
+    Use assert to verify that both arguments are integers greater than 0.
+    Use index_range to find the correct indexes to paginate the dataset
+    correctly and return the appropriate page of the dataset
+    (i.e. the correct list of rows).
+    If the input arguments are out of range for the dataset,
+    an empty list should be returned
+"""
+
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    Calculate the start and end index for a given page and page size.
+
+    Args:
+    page (int): The page number (1-indexed).
+    page_size (int): The number of items per page.
+
+    Returns:
+    Tuple: A tuple containing the start index and end index.
+    """
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        data = self.dataset()
+
+        try:
+            start, end = index_range(page, page_size)
+            return dara[start:end]
+        except IndexError:
+            return []
