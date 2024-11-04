55b93cc50a0fde31479d320b0e5c959d21a59e9f
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-11-04 17:51:36 +0000

    Updated solution

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 """
-    Create a class LFUCache that inherits from BaseCaching and is a caching system:
+    Create a class LFUCache that inherits from BaseCaching and is a
+    caching system:
 
-You must use self.cache_data - dictionary from the parent class BaseCaching
-You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the item value for the key key.
-If key or item is None, this method should not do anything.
-If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-you must discard the least frequency used item (LFU algorithm)
-if you find more than 1 item to discard, you must use the LRU algorithm to discard only the least recently used
-you must print DISCARD: with the key discarded and following by a new line
-def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in self.cache_data, return None
+    You must use self.cache_data - dictionary from the parent class
+    BaseCaching
+    You can overload def __init__(self): but don’t forget to call
+    the parent init: super().__init__()
+    def put(self, key, item):
+    Must assign to the dictionary self.cache_data the item value
+    for the key key.
+    If key or item is None, this method should not do anything.
+    If the number of items in self.cache_data is higher that
+    BaseCaching.MAX_ITEMS:
+    you must discard the least frequency used item (LFU algorithm)
+    if you find more than 1 item to discard, you must use the LRU algorithm
+    to discard only the least recently used
+    you must print DISCARD: with the key discarded and following by a new line
+    def get(self, key):
+    Must return the value in self.cache_data linked to key.
+    If key is None or if the key doesn’t exist in self.cache_data,
+    return None
 """
 
 
@@ -47,12 +54,11 @@ class LFUCache(BaseCaching):
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 # Find the least frequently used item(s)
                 min_freq = min(self.freq_map.values())
-                lfu_keys = [k for k, v in self.freq_map.items() /
-                            if v == min_freq]
+                lfu_keys = [k for k, v in self.freq_map.items() if v == min_freq]
 
-        """ If there are multiple candidates, apply LRU to 
-        decide which to discard
-        """
+                """ If there are multiple candidates, apply LRU to
+                decide which to discard
+                """
                 lfu_key = None
                 for key in self.use_order:
                     if key in lfu_keys:
