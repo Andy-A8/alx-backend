ae84a1ba3d54d22246d0ebed26131b3ec440ae14
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-11-10 09:30:07 +0000

    Updated solution

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def helloworld():
     """ Function to output desired results """
-    return render_template('0index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
